a3f8f0c4257ff5f08e2685e1c1436653dbfa287a
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-07-11 17:19:40 +0530

    input size and create new array

--- Lab Session 2/src/com/ashutosh/transaction/main/Driver.java
+++ Lab Session 2/src/com/ashutosh/transaction/main/Driver.java
@@ -1,11 +1,20 @@
 package com.ashutosh.transaction.main;
 
+import java.util.Scanner;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		
 		int[] arr;
 		int size, targets, targetValue;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the size of transaction array");
+		size = sc.nextInt();
+		
+		arr = new int[size];
 		
 		
 
