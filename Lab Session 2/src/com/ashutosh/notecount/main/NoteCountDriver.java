3d0347e6ac5207e449a975ea0b679e3920388969
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-07-11 22:47:49 +0530

    input values in array

--- Lab Session 2/src/com/ashutosh/notecount/main/NoteCountDriver.java
+++ Lab Session 2/src/com/ashutosh/notecount/main/NoteCountDriver.java
@@ -16,6 +16,12 @@ public class NoteCountDriver {
 		
 		int[] notes = new int[size];
 		
+		System.out.println("Enter the currency denominations value");
+		
+		for(int i=0; i<size; ++i) {
+			notes[i] = sc.nextInt();
+		}
+		
 		
 
 	}
