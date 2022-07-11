021a2d8cc81a691b07782e2d89378e4cb70c2909
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-07-12 00:10:24 +0530

    improve display

--- Lab Session 2/src/com/ashutosh/notecount/service/NotesCount.java
+++ Lab Session 2/src/com/ashutosh/notecount/service/NotesCount.java
@@ -10,15 +10,14 @@ public class NotesCount {
 			for(int i=0; i<notes.length; ++i) {
 				if(amount >= notes[i]) {
 					notesCounter[i] = amount/notes[i];
-					int remainder = amount % notes[i];
-					amount = amount - remainder;
+					amount = amount - notesCounter[i]*notes[i];
 				}
 			}
 			if(amount>0) {
 				System.out.println("exact amount cannot be given with the highest denomination");
 			}
 			else {
-				System.out.println("Your payment approach in order to give min no of notes will be");
+				System.out.println("\nYour payment approach in order to give min no of notes will be");
 				for(int i=0; i<notes.length; ++i) {
 					if(notesCounter[i] != 0) {
 						System.out.println(notes[i] + ":" + notesCounter[i]);
