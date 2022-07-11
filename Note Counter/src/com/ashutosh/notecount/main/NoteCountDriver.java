df74292f8a27b63eea645d4e4fa59819dab6657d
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-07-12 00:14:20 +0530

    improve code

--- Note Counter/src/com/ashutosh/notecount/main/NoteCountDriver.java
+++ Note Counter/src/com/ashutosh/notecount/main/NoteCountDriver.java
@@ -7,7 +7,7 @@ import com.ashutosh.notecount.service.NotesCount;
 
 public class NoteCountDriver {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		
 		MergeSort msObj = new MergeSort();
 		NotesCount ncObj = new NotesCount();
