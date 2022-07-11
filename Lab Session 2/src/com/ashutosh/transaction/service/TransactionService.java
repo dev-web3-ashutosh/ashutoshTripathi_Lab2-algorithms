15a1aea2f092bd3865c3a4e021205203dc892dab
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-07-11 18:18:05 +0530

    define checkTarget method

--- Lab Session 2/src/com/ashutosh/transaction/service/TransactionService.java
+++ Lab Session 2/src/com/ashutosh/transaction/service/TransactionService.java
@@ -1,8 +1,32 @@
 package com.ashutosh.transaction.service;
 
+import java.util.Scanner;
+
 public class TransactionService {
 	
+	Scanner sc = new Scanner(System.in);
+	
 	public void checkTarget(int[] array, int noOfTargets) {
+		
+		for(int x=0; x<noOfTargets; ++x) {
+			int value, sum=0;
+			boolean achieved = false; //to know if target has been achieved
+			
+			System.out.println("Enter the value of target");
+			value = sc.nextInt(); //input target to be searched
+			
+			for(int i=0; i<array.length; ++i) {
+				sum += array[i];
+				if(sum >= value) {
+					System.out.println("Target achieved after " + (i+1) + " transactions");
+					achieved = true;
+					break;
+				}
+			}
+			if(achieved == false) {
+				System.out.println("Given target is not achieved");
+			}
+		}
 		
 	}
 
