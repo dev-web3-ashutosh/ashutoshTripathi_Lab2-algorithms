e27d4c94bd0e243e1de757d79cda48b11699048d
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-07-12 00:12:59 +0530

    create new project

---
+++ Note Counter/src/com/ashutosh/notecount/main/NoteCountDriver.java
@@ -0,0 +1,38 @@
+package com.ashutosh.notecount.main;
+
+import java.util.Scanner;
+
+import com.ashutosh.notecount.service.MergeSort;
+import com.ashutosh.notecount.service.NotesCount;
+
+public class NoteCountDriver {
+
+	public static void main(String[] args) throws Exception {
+		
+		MergeSort msObj = new MergeSort();
+		NotesCount ncObj = new NotesCount();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the size of currency denominations");
+		int size = sc.nextInt();
+		
+		int[] notes = new int[size];
+		
+		System.out.println("Enter the currency denominations value");
+		
+		for(int i=0; i<size; ++i) {
+			notes[i] = sc.nextInt();
+		}
+		
+		System.out.println("Enter the amount you want to pay");
+		int amount = sc.nextInt();
+		
+		msObj.sort(notes, 0, notes.length-1);
+		ncObj.noteCounter(notes, amount);
+		
+		sc.close();
+
+	}
+
+}
