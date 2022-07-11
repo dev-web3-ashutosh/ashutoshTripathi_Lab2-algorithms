f557058ad1561fcc74ad4dbc5fbb96b045d404a2
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-07-11 17:49:14 +0530

    add comments

--- Lab Session 2/src/com/ashutosh/transaction/main/Driver.java
+++ Lab Session 2/src/com/ashutosh/transaction/main/Driver.java
@@ -16,19 +16,24 @@ public class Driver {
 		System.out.println("Enter the size of transaction array");
 		size = sc.nextInt();
 		
-		arr = new int[size];
+		arr = new int[size]; //create new array of size input by user
 		
+		//input array values
 		for(int i=0; i<size; ++i) {
 			arr[i] = sc.nextInt();
 		}
 		
+		//input number of targets to be searched
 		System.out.println("Enter the total no of targets that needs to be achieved");
 		targets = sc.nextInt();
 		
+		//create object of TransactionService class
 		TransactionService obj = new TransactionService();
 		
+		//invoke checktarget method
 		obj.checkTarget(arr, targets);
 		
+		//close scanner
 		sc.close();
 
 	}
