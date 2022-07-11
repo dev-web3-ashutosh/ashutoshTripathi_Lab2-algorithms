b128740f650bdf897bf303a95b24ebdbd50e3a17
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-07-11 22:43:42 +0530

    create scanner object

--- Lab Session 2/src/com/ashutosh/notecount/main/NoteCountDriver.java
+++ Lab Session 2/src/com/ashutosh/notecount/main/NoteCountDriver.java
@@ -1,11 +1,15 @@
 package com.ashutosh.notecount.main;
 
+import java.util.Scanner;
+
 public class NoteCountDriver {
 
 	public static void main(String[] args) {
 		
 		MergeSort msObj = new MergeSort();
 		NotesCount ncObj = new NotesCount();
+		
+		Scanner sc = new Scanner(System.in);
 		
 		
 
